34d61caf8129fa82205a2d9cbd783a0398aff0a6
Steven Xu <stevenxxiu@gmail.com>
2017-03-20 19:41:16 +1100

    implemented hierarchical_softmax & hierarchical_softmax_cross_entropy

--- main.py
+++ main.py
@@ -30,24 +30,41 @@ def tree_to_paths(tree):
     for node, children in tree.items():
         path = node_to_path.pop(node)
         for i, child in enumerate(children):
-            node_to_path[child] = path + [(node, i)]
+            node_to_path[child] = path + [(node, -1 if i == 0 else 1)]
     return node_to_path
 
 
 def tree_to_mat(tree, word_to_index):
     '''
-    Converts a huffman tree to a path matrix where each row contains the path from the root with -1 representing nop,
-    a child matrix where 0 and 1 represent the left and right child respectively.
+    Converts a huffman tree to a path matrix where each row contains the path from the root, a child matrix where 0
+    and 1 represent the left and right child respectively.
     '''
     word_to_path = tree_to_paths(tree)
     d = max(len(path) for path in word_to_path.values())
-    path_matrix = np.empty((len(word_to_index), d), dtype=int)
-    path_matrix.fill(-1)
-    child_matrix = np.zeros((len(word_to_index), d))
+    mask_mat = np.zeros((len(word_to_index), d))
+    path_mat = np.zeros((len(word_to_index), d), dtype=int)
+    child_mat = np.zeros((len(word_to_index), d))
     for word, path in word_to_path.items():
-        path_matrix[word_to_index[word], :len(path)] = [path_item[0] for path_item in path]
-        child_matrix[word_to_index[word], :len(path)] = [path_item[1] for path_item in path]
-    return path_matrix, child_matrix
+        mask_mat[word_to_index[word], :len(path)] = 1
+        path_mat[word_to_index[word], :len(path)] = [path_item[0] for path_item in path]
+        child_mat[word_to_index[word], :len(path)] = [path_item[1] for path_item in path]
+    return mask_mat, path_mat, child_mat
+
+
+def hierarchical_softmax(context, W, b, mask_mat, path_mat, child_mat):
+    # sparse matrices are not implemented on the gpu yet
+    return T.sum(mask_mat.dimshuffle(('x', 0, 1)) * T.log(T.nnet.sigmoid(
+        child_mat.dimshuffle(('x', 0, 1)) *
+        (T.dot(W[path_mat], context.T).dimshuffle((2, 0, 1)) + b.dimshuffle((0, 'x')))
+    )), axis=-1)
+
+
+def hierarchical_softmax_cross_entropy(context, W, b, mask_mat, path_mat, child_mat, target):
+    # sparse matrices are not implemented on the gpu yet
+    return -T.sum(mask_mat[target].dimshuffle(('x', 0, 1)) * T.log(T.nnet.sigmoid(
+        child_mat[target].dimshuffle(('x', 0, 1)) *
+        (T.dot(W[path_mat[target]], context.T).dimshuffle((2, 0, 1)) + b[target].dimshuffle((0, 'x')))
+    )), axis=-1)
 
 
 def words_to_mat(words, step_size, word_to_index):
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import unittest
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+from scipy.special import expit
 
 from main import *
 
@@ -14,34 +15,96 @@ class TestHuffman(unittest.TestCase):
 
     def test_tree_to_paths(self):
         self.assertEqual(tree_to_paths({0: ['a', 1], 1: [3, 2], 3: ['e', 'c'], 2: ['d', 'b']}), {
-            'a': [(0, 0)],
+            'a': [(0, -1)],
             'b': [(0, 1), (1, 1), (2, 1)],
-            'c': [(0, 1), (1, 0), (3, 1)],
-            'd': [(0, 1), (1, 1), (2, 0)],
-            'e': [(0, 1), (1, 0), (3, 0)],
+            'c': [(0, 1), (1, -1), (3, 1)],
+            'd': [(0, 1), (1, 1), (2, -1)],
+            'e': [(0, 1), (1, -1), (3, -1)],
         })
 
     def test_tree_to_mat(self):
-        path_matrix, child_matrix = tree_to_mat(
+        mask_mat, path_mat, child_mat = tree_to_mat(
             {0: ['a', 1], 1: [3, 2], 3: ['e', 'c'], 2: ['d', 'b']},
             {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4}
         )
-        assert_array_equal(path_matrix, np.array([
-            [0, -1, -1],
+        assert_array_equal(mask_mat, np.array([
+            [1, 0, 0],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]))
+        assert_array_equal(path_mat, np.array([
+            [0, 0, 0],
             [0, 1, 2],
             [0, 1, 3],
             [0, 1, 2],
             [0, 1, 3],
         ], dtype=int))
-        assert_array_equal(child_matrix, np.array([
-            [0, 0, 0],
+        assert_array_equal(child_mat, np.array([
+            [-1, 0, 0],
             [1, 1, 1],
-            [1, 0, 1],
-            [1, 1, 0],
-            [1, 0, 0],
+            [1, -1, 1],
+            [1, 1, -1],
+            [1, -1, -1],
         ]))
 
 
-# XXX test that hierarchical softmax is correct for the huffman tree
+class TestHierarchicalSoftmax(unittest.TestCase):
+    def test_hierarchical_softmax(self):
+        context = T.matrix()
+        W = T.matrix()
+        b = T.vector()
+        mask_mat, path_mat, child_mat = tree_to_mat(
+            {0: ['a', 1], 1: [3, 2], 3: ['e', 'c'], 2: ['d', 'b']},
+            {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4}
+        )
+        res = theano.function(
+            [context, W, b],
+            hierarchical_softmax(
+                context, W, b, theano.shared(mask_mat), theano.shared(path_mat), theano.shared(child_mat)
+            ), on_unused_input='ignore'
+        )(
+            np.array([[.01, .02], [.03, .04]], dtype=np.float32),
+            np.array([[.05, .06], [.07, .08], [.09, .10], [.11, .12], [.13, .14]], dtype=np.float32),
+            np.array([.15, .16, .17, .18, .19], dtype=np.float32)
+        )
+        assert_array_almost_equal(res[0][0], np.array([
+            np.log(1 - expit(np.dot([.01, .02], [.05, .06]) + 0.15))
+        ]), decimal=4)
 
-# XXX test that the cross entropy calculation is correct for huffman trees
+    def test_hierarchical_softmax_cross_entropy(self):
+        context = T.matrix()
+        W = T.matrix()
+        b = T.vector()
+        target = T.ivector()
+        mask_mat, path_mat, child_mat = tree_to_mat(
+            {0: ['a', 1], 1: [3, 2], 3: ['e', 'c'], 2: ['d', 'b']},
+            {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4}
+        )
+        res = theano.function(
+            [context, W, b, target],
+            hierarchical_softmax_cross_entropy(
+                context, W, b, theano.shared(mask_mat), theano.shared(path_mat), theano.shared(child_mat), target
+            )
+        )(
+            np.array([[.01, .02], [.03, .04]], dtype=np.float32),
+            np.array([[.05, .06], [.07, .08], [.09, .10], [.11, .12], [.13, .14]], dtype=np.float32),
+            np.array([.15, .16, .17, .18, .19], dtype=np.float32),
+            np.array([0, 1], dtype=np.int32)
+        )
+        assert_array_almost_equal(res, np.array([[
+            -np.log(1 - expit(np.dot([.01, .02], [.05, .06]) + 0.15)),
+            -(
+                np.log(expit(np.dot([.01, .02], [.05, .06]) + 0.15)) +
+                np.log(expit(np.dot([.01, .02], [.07, .08]) + 0.16)) +
+                np.log(expit(np.dot([.01, .02], [.09, .10]) + 0.17))
+            )
+        ], [
+            -np.log(1 - expit(np.dot([.03, .04], [.05, .06]) + 0.15)),
+            -(
+                np.log(expit(np.dot([.03, .04], [.05, .06]) + 0.15)) +
+                np.log(expit(np.dot([.03, .04], [.07, .08]) + 0.16)) +
+                np.log(expit(np.dot([.03, .04], [.09, .10]) + 0.17))
+            )
+        ]]), decimal=4)
