b23fbe399fc5502e80a13dfdaae05939305aab10
Steven Xu <stevenxxiu@gmail.com>
2017-03-30 15:51:24 +1100

    gensim, same result as tensorflow

---
+++ dist_rep_sent_doc/main_gensim.py
@@ -0,0 +1,42 @@
+import logging
+import os
+from collections import Counter
+
+import joblib
+import numpy as np
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
+from sklearn.linear_model import LogisticRegression
+
+from dist_rep_sent_doc.data import preprocess_data
+
+memory = joblib.Memory('__cache__', verbose=0)
+
+
+@memory.cache
+def gen_data(path):
+    return sum([[
+        [TaggedDocument(doc[1], [i]) for i, doc in enumerate(docs)],
+        [doc[0] for doc in docs]
+    ] for docs in preprocess_data(path)], [])
+
+
+def main():
+    # fit
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    train_X, train_y, val_X, val_y, test_X, test_y = gen_data('../data/stanford_sentiment_treebank/class_5')
+    model = Doc2Vec(train_X + val_X + test_X, size=400, window=8, min_count=0, workers=4, dm=1, dm_concat=1, hs=1)
+    model.train(train_X)
+
+    # save
+    os.makedirs('__cache__/gensim', exist_ok=True)
+    model.save('__cache__/gensim/pvdm.model')
+
+    # logreg
+    model = Doc2Vec.load('__cache__/gensim/pvdm.model')
+    logreg = LogisticRegression()
+    logreg.fit(np.array(model.docvecs), train_y)
+    print(Counter(logreg.predict(np.array(model.docvecs))))
+
+if __name__ == '__main__':
+    main()
