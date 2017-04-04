numerical precision fix

dist_rep_sent_doc/layers.py
@@ -70,11 +70,11 @@ class HierarchicalSoftmaxLayer(base._Layer):
             indices = tf.boolean_mask(tf.reshape(
                 tf.tile(tf.reshape(tf.range(tf.shape(input_)[0]), [-1, 1]), [1, self.nodes.shape.as_list()[1]]), [-1]
             ), masks)
-            return tf.reduce_sum(tf.log(tf.nn.sigmoid(
-                signs * tf.reduce_sum(tf.gather(self.W, nodes) * tf.gather(input_, indices), 1)
-            ))) / tf.cast(tf.shape(input_)[0], tf.float32)
+            return tf.reduce_sum(-tf.nn.softplus(
+                -signs * tf.reduce_sum(tf.gather(self.W, nodes) * tf.gather(input_, indices), 1)
+            )) / tf.cast(tf.shape(input_)[0], tf.float32)
         else:
             node_outputs = tf.matmul(inputs, tf.transpose(self.W))
             return tf.transpose(tf.sparse_tensor_dense_matmul(self.output_index, tf.transpose(
-                tf.log(tf.nn.sigmoid(tf.concat([node_outputs, -node_outputs], 1)))
+                -tf.nn.softplus(-tf.concat([node_outputs, -node_outputs], 1))
             )))