dac955379558016a88625541c54d162995672957
Steven Xu <stevenxxiu@gmail.com>
2017-04-25 20:36:32 +1000

    we get better results using gradient descent for logreg than adadelta; report losses

--- dist_rep_sent_doc/main.py
+++ dist_rep_sent_doc/main.py
@@ -80,7 +80,7 @@ def rolling_window(a, window):
 
 # noinspection PyTypeChecker
 def run_pvdm(
-    name, docs, word_to_freq, word_to_index, tree, word_to_prob, training_, window_size, embedding_size, cur_lr, min_lr,
+    name, docs, word_to_freq, word_to_index, tree, word_to_prob, training_, window_size, embedding_size, start_lr, end_lr,
     batch_size, epoch_size, train_model_path=None
 ):
     # network
@@ -122,7 +122,6 @@ def run_pvdm(
             saver.restore(sess, os.path.join(train_model_path, 'model.ckpt'))
 
         # train
-        lr_delta = (cur_lr - min_lr) / epoch_size
         print(datetime.datetime.now(), 'started training')
         for i in range(epoch_size):
             X_doc_, X_words_, y_ = [], [], []
@@ -140,11 +139,11 @@ def run_pvdm(
             X_words_ = np.vstack(X_words_)
             y_ = np.concatenate(y_)
             p = np.random.permutation(len(y_))
+            cur_lr = start_lr - (start_lr - end_lr) / (epoch_size - 1) * i
             for j in range(0, len(y_), batch_size):
                 k = p[j:j + batch_size]
                 sess.run(train_op, feed_dict={X_doc: X_doc_[k], X_words: X_words_[k], y: y_[k], lr: cur_lr})
             print(datetime.datetime.now(), f'finished epoch {i}')
-            cur_lr -= lr_delta
 
         # save
         path = os.path.join('__cache__', 'tf', f'pvdm-{name}-{uuid.uuid4()}')
@@ -155,7 +154,7 @@ def run_pvdm(
 
 # noinspection PyTypeChecker
 def run_dbow(
-    name, docs, word_to_freq, word_to_index, tree, word_to_prob, training_, embedding_size, cur_lr, min_lr,
+    name, docs, word_to_freq, word_to_index, tree, word_to_prob, training_, embedding_size, start_lr, end_lr,
     batch_size, epoch_size, train_model_path=None
 ):
     # network
@@ -189,7 +188,6 @@ def run_dbow(
             saver.restore(sess, os.path.join(train_model_path, 'model.ckpt'))
 
         # train
-        lr_delta = (cur_lr - min_lr) / epoch_size
         print(datetime.datetime.now(), 'started training')
         for i in range(epoch_size):
             X_doc_, y_ = [], []
@@ -201,11 +199,11 @@ def run_dbow(
             X_doc_ = np.concatenate(X_doc_)
             y_ = np.concatenate(y_)
             p = np.random.permutation(len(y_))
+            cur_lr = start_lr - (start_lr - end_lr) / (epoch_size - 1) * i
             for j in range(0, len(y_), batch_size):
                 k = p[j:j + batch_size]
                 sess.run(train_op, feed_dict={X_doc: X_doc_[k], y: y_[k], lr: cur_lr})
             print(datetime.datetime.now(), f'finished epoch {i}')
-            cur_lr -= lr_delta
 
         # save
         path = os.path.join('__cache__', 'tf', f'dbow-{name}-{uuid.uuid4()}')
@@ -231,22 +229,26 @@ def load_docvecs(train, test, pvdm_train_path, pvdm_test_path, dbow_train_path, 
 
 
 # noinspection PyTypeChecker
-def run_nn(X_train, y_train, X_test, y_test, embedding_size, layer_sizes, batch_size, epoch_size):
+def run_nn(X_train, y_train, X_test, y_test, embedding_size, layer_sizes, start_lr, end_lr, batch_size, epoch_size):
     X = tf.placeholder(tf.float32, [None, embedding_size])
     y = tf.placeholder(tf.int32, [None])
+    lr = tf.placeholder(tf.int32, [])
     dense = X
     for layer_size in layer_sizes:
         dense = tf.layers.dense(dense, layer_size, kernel_initializer=init_ops.glorot_uniform_initializer())
     loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=dense, labels=y))
-    train_op = tf.train.AdadeltaOptimizer(learning_rate=50).minimize(loss)
+    train_op = tf.train.GradientDescentOptimizer(learning_rate=lr).minimize(loss)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for i in range(epoch_size):
             p = np.random.permutation(len(y_train))
+            total_loss = 0
+            cur_lr = start_lr - (start_lr - end_lr) / (epoch_size - 1) * i
             for j in range(0, len(y_train), batch_size):
                 k = p[j:j + batch_size]
-                _, batch_loss = sess.run([train_op, loss], feed_dict={X: X_train[k], y: y_train[k]})
-            print(datetime.datetime.now(), f'finished epoch {i}, batch loss: {batch_loss:.5f}')
+                _, batch_loss = sess.run([train_op, loss], feed_dict={X: X_train[k], y: y_train[k], lr: cur_lr})
+                total_loss += len(k) * batch_loss
+            print(datetime.datetime.now(), f'finished epoch {i}, loss: {total_loss / len(y_train):f}')
         y_pred = sess.run(tf.argmax(dense, axis=1), feed_dict={X: X_test})
         corrects = sum(y_pred == y_test)
         print(f'error rate: {(len(y_pred) - corrects) / len(y_pred)}')
@@ -262,29 +264,29 @@ def main():
         # pvdm
         pvdm_train_path = run_pvdm(
             f'{name}_train', train, *tables, training_=True,
-            window_size=5, embedding_size=100, cur_lr=0.025, min_lr=0.001, batch_size=512, epoch_size=20
+            window_size=5, embedding_size=100, start_lr=0.025, end_lr=0.001, batch_size=512, epoch_size=20
         )
         pvdm_test_path = run_pvdm(
             f'{name}_test', test, *tables, training_=False,
-            window_size=5, embedding_size=100, cur_lr=0.1, min_lr=0.0001, batch_size=2048, epoch_size=5,
+            window_size=5, embedding_size=100, start_lr=0.1, end_lr=0.0001, batch_size=2048, epoch_size=5,
             train_model_path=pvdm_train_path
         )
 
         # dbow
         dbow_train_path = run_dbow(
             f'{name}_train', train, *tables, training_=True,
-            embedding_size=100, cur_lr=0.025, min_lr=0.001, batch_size=512, epoch_size=20
+            embedding_size=100, start_lr=0.025, end_lr=0.001, batch_size=512, epoch_size=20
         )
         dbow_test_path = run_dbow(
             f'{name}_test', test, *tables, training_=False,
-            embedding_size=100, cur_lr=0.1, min_lr=0.0001, batch_size=2048, epoch_size=5,
+            embedding_size=100, start_lr=0.1, end_lr=0.0001, batch_size=2048, epoch_size=5,
             train_model_path=dbow_train_path
         )
 
         # classify
         run_nn(
             *load_docvecs(train, test, pvdm_train_path, pvdm_test_path, dbow_train_path, dbow_test_path),
-            embedding_size=200, layer_sizes=[2], batch_size=2048, epoch_size=10
+            embedding_size=200, layer_sizes=[2], start_lr=5, end_lr=0.1, batch_size=2048, epoch_size=20
         )
 
     elif name in ('sstb_2', 'sstb_5'):
@@ -297,29 +299,30 @@ def main():
         # pvdm
         pvdm_train_path = run_pvdm(
             f'{name}_train', train, *tables, training_=True,
-            window_size=4, embedding_size=100, cur_lr=0.025, min_lr=0.001, batch_size=512, epoch_size=20
+            window_size=4, embedding_size=100, start_lr=0.025, end_lr=0.001, batch_size=512, epoch_size=20
         )
         pvdm_test_path = run_pvdm(
             f'{name}_test', test, *tables, training_=False,
-            window_size=4, embedding_size=100, cur_lr=0.1, min_lr=0.0001, batch_size=2048, epoch_size=5,
+            window_size=4, embedding_size=100, start_lr=0.1, end_lr=0.0001, batch_size=2048, epoch_size=5,
             train_model_path=pvdm_train_path
         )
 
         # dbow
         dbow_train_path = run_dbow(
             f'{name}_train', train, *tables, training_=True,
-            embedding_size=100, cur_lr=0.025, min_lr=0.001, batch_size=512, epoch_size=20
+            embedding_size=100, start_lr=0.025, end_lr=0.001, batch_size=512, epoch_size=20
         )
         dbow_test_path = run_dbow(
             f'{name}_test', test, *tables, training_=False,
-            embedding_size=100, cur_lr=0.1, min_lr=0.0001, batch_size=2048, epoch_size=5,
+            embedding_size=100, start_lr=0.1, end_lr=0.0001, batch_size=2048, epoch_size=5,
             train_model_path=dbow_train_path
         )
 
         # classify
         run_nn(
             *load_docvecs(train, test, pvdm_train_path, pvdm_test_path, dbow_train_path, dbow_test_path),
-            embedding_size=200, layer_sizes=[(2 if name == 'sstb_2' else 5)], batch_size=2048, epoch_size=10
+            embedding_size=200, layer_sizes=[(2 if name == 'sstb_2' else 5)],
+            start_lr=5, end_lr=0.1, batch_size=2048, epoch_size=10
         )
 
 if __name__ == '__main__':
