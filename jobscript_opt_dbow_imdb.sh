7f225fcff2f306a2229b8abffa64521786879d3e
Steven Xu <stevenxxiu@gmail.com>
2017-05-21 17:31:44 +1000

    allow optimizing sstb as well in the same script

--- jobscript_opt_dbow_imdb.sh
+++ jobscript_opt_dbow_imdb.sh
@@ -8,8 +8,8 @@ module load tensorflow/1.0.1-python3.5
 cd /short/cp1/sx6361/dist_rep_sent_doc
 python=/home/563/sx6361/.pyenv/versions/3.6.1/bin/python
 export PYTHONPATH="/home/563/sx6361/.pyenv/versions/3.6.1/lib/python3.6/site-packages:$PWD"
-CUDA_VISIBLE_DEVICES=0 "$python" dist_rep_sent_doc/hyperopt/imdb_dbow.py &
+CUDA_VISIBLE_DEVICES=0 "$python" dist_rep_sent_doc/hyperopt/dbow.py imdb &
 P1=$!
-CUDA_VISIBLE_DEVICES=1 "$python" dist_rep_sent_doc/hyperopt/imdb_dbow.py &
+CUDA_VISIBLE_DEVICES=1 "$python" dist_rep_sent_doc/hyperopt/dbow.py imdb &
 P2=$!
 wait $P1 $P2
