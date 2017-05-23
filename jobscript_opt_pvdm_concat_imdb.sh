e80e2d490d0405add27b2f44184c1d2930bff3b1
Steven Xu <stevenxxiu@gmail.com>
2017-05-23 15:08:49 +1000

    use pascal since concat keeps running out of gpu memory

--- jobscript_opt_pvdm_concat_imdb.sh
+++ jobscript_opt_pvdm_concat_imdb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#PBS -q gpu
-#PBS -l ngpus=2
+# use pascal due to the possibility of exceeding gpu memory with some settings
+#PBS -q gpupascal
+#PBS -l ngpus=1
 #PBS -l ncpus=6
 #PBS -l mem=32GB
 #PBS -l walltime=20:00:00
@@ -8,8 +9,4 @@ module load tensorflow/1.0.1-python3.5
 cd /short/cp1/sx6361/dist_rep_sent_doc
 python=/home/563/sx6361/.pyenv/versions/3.6.1/bin/python
 export PYTHONPATH="/home/563/sx6361/.pyenv/versions/3.6.1/lib/python3.6/site-packages:$PWD"
-CUDA_VISIBLE_DEVICES=0 "$python" dist_rep_sent_doc/hyperopt/pvdm.py concat imdb &
-P1=$!
-CUDA_VISIBLE_DEVICES=1 "$python" dist_rep_sent_doc/hyperopt/pvdm.py concat imdb &
-P2=$!
-wait $P1 $P2
+"$python" dist_rep_sent_doc/hyperopt/pvdm.py concat imdb
