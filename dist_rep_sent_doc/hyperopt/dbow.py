8ce3802a32540dddb40d0d5aae2060b1268fb282
Steven Xu <stevenxxiu@gmail.com>
2017-05-26 14:35:49 +1000

    dbow sg hyperparameter optimization

--- dist_rep_sent_doc/hyperopt/dbow.py
+++ dist_rep_sent_doc/hyperopt/dbow.py
@@ -11,9 +11,11 @@ from contextlib import suppress
 
 def main():
     arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('sg', type=bool)
     arg_parser.add_argument('dataset')
     args = arg_parser.parse_args()
     all_params = {
+        'sg': list(range(2, 6 + 1)),
         'embedding_size': [50, 100, 150, 200, 250, 300, 350, 400],
         'min_freq': [0, 1, 2, 3, 4, 5],
         'sample': [1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5],
@@ -23,19 +25,19 @@ def main():
         'val_epoch_size': list(range(1, 50 + 1)),
     }
     for i in itertools.count(0):
-        name = f'results/dbow_{args.dataset}_{i:03d}.txt'
+        name = f'results/dbow_{"sg_" if args.sg else ""}{args.dataset}_{i:03d}.txt'
         with suppress(FileExistsError), open(name, 'x'), open(name, 'a', encoding='utf-8') as out:
             p = {name: random.choice(values) for name, values in all_params.items()}
             train_path = f'__cache__/tf/{uuid.uuid4()}'
             test_path = f'__cache__/tf/{uuid.uuid4()}'
             subprocess.call([sys.executable, 'dist_rep_sent_doc/main.py', args.dataset, 'val', 'dbow', json.dumps({
-                'embedding_size': p['embedding_size'], 'min_freq': p['min_freq'], 'sample': p['sample'],
-                'lr': p['train_lr'], 'batch_size': 2048, 'epoch_size': p['train_epoch_size'],
+                'sg': p['sg'] if args.sg else 0, 'embedding_size': p['embedding_size'], 'min_freq': p['min_freq'],
+                'sample': p['sample'], 'lr': p['train_lr'], 'batch_size': 2048, 'epoch_size': p['train_epoch_size'],
                 'save_path': train_path
             })], stdout=out)
             subprocess.call([sys.executable, 'dist_rep_sent_doc/main.py', args.dataset, 'val', 'dbow', json.dumps({
-                'embedding_size': p['embedding_size'], 'min_freq': p['min_freq'], 'sample': p['sample'],
-                'lr': p['val_lr'], 'batch_size': 2048, 'epoch_size': p['val_epoch_size'],
+                'sg': p['sg'] if args.sg else 0, 'embedding_size': p['embedding_size'], 'min_freq': p['min_freq'],
+                'sample': p['sample'], 'lr': p['val_lr'], 'batch_size': 2048, 'epoch_size': p['val_epoch_size'],
                 'save_path': test_path, 'train_path': train_path
             })], stdout=out)
             subprocess.call([sys.executable, 'dist_rep_sent_doc/main.py', args.dataset, 'val', 'nn', json.dumps({
