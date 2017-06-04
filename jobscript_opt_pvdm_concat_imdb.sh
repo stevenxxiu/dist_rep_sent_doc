96046e25e2cde7bc3d74b625dd1675a52b88963d
Steven Xu <stevenxxiu@gmail.com>
2017-06-04 18:20:54 +1000

    path update

--- jobscript_opt_pvdm_concat_imdb.sh
+++ jobscript_opt_pvdm_concat_imdb.sh
@@ -5,7 +5,7 @@
 #PBS -l mem=32GB
 #PBS -l walltime=20:00:00
 module load tensorflow/1.0.1-python3.5
-cd /short/cp1/sx6361/dist_rep_sent_doc
+cd /short/cp1/sx6361/projects/dist_rep_sent_doc
 python=/home/563/sx6361/.pyenv/versions/3.6.1/bin/python
 export PYTHONPATH="/home/563/sx6361/.pyenv/versions/3.6.1/lib/python3.6/site-packages:$PWD"
 export PYTHONUNBUFFERED=1
